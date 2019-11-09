3591f0ec5d7879b0e78da607a144acd49299da73
Max <maksymzawrotny@wp.pl>
2019-11-09 21:50:10 +0100

    Init commit. Data load

---
+++ R - PROJEKT GRUPOWY I.R
@@ -0,0 +1,6 @@
+#za³adowanie bazowych danych
+basedata <- read.csv("E:/9sem/DM/zep/bank/bank.csv", row.names=NULL, sep=";", stringsAsFactors=FALSE)
+
+#opis danych i zagadnienia klasyfikacyjnego:
+#zaczerpniêty z MCI Machine Learning Repository:
+
