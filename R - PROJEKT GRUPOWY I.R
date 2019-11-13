3c3374bbdff79f099e504a9c54f40d95130f18fb
Max <maksymzawrotny@wp.pl>
2019-11-13 23:26:06 +0100

    Skończona analiza eksploracyjna

--- R - PROJEKT GRUPOWY I.R
+++ R - PROJEKT GRUPOWY I.R
@@ -1,7 +1,7 @@
 #za³adowanie potrzebnych zale¿noœci
 library(sqldf)
 library(data.table)
-
+library(plyr)
 #za³adowanie bazowych danych
 basedata <- read.csv("E:/9sem/DM/zep/bank/bank.csv", row.names=NULL, sep=";", stringsAsFactors=FALSE)
 
@@ -60,9 +60,11 @@ order by age")
 
 par(mfrow = c(1,3))
 plot(age_no$age, age_no$no, xlab = "Wiek",
-     ylab = "Liczba odmów")
-plot(age_yes$age, age_yes$yes, xlab = "Wiek", ylab = "Liczba zgód")
-plot(age_y$age, age_y$liczba_prob, xlab = "Wiek", ylab = "Liczba prób")
+     ylab = "Liczba odmów", main = "Rozk³ad wieku odmawiaj¹cych")
+plot(age_yes$age, age_yes$yes, xlab = "Wiek", ylab = "Liczba zgód",
+     main="Rozk³ad wieku zgadzajacych siê")
+plot(age_y$age, age_y$liczba_prob, xlab = "Wiek", ylab = "Liczba prób",
+     main = "Rozk³ad wieku ogólnie")
 #strasznie ma³o danych dla ludzi skrajnie m³odych i skrajnie starych
 #dlatego nie ma wykresu skutecznoœæi w u³amku, bo dla tych ludzi
 #wynik by³by niewiarygodny i zawy¿ony
@@ -88,7 +90,9 @@ job_y <- data.frame(row.names = job_yes$job)
 job_y$skutecznosc <- job_yes$yes/(job_yes$yes + job_no$no)
 
 par(mfrow = c(1,1))
-barplot(job_y$skutecznosc, names.arg = rownames(job_y))
+barplot(job_y$skutecznosc, names.arg = rownames(job_y), 
+        ylab = "Skutecznoœæ marketingu",
+        main = "Skutecznoœæ marketingu ze wzglêdu na zawód")
 #zdecydowanie du¿a wiêksza skutecznoœæ dla studentów/uczniów i emerytów
 #ta zmienna te¿ bêdzie istotna w modelowaniu
 
@@ -117,7 +121,9 @@ marital = sqldf(
 marital$skutecznosc <- marital$udane / marital$liczba_prob
 
 par(mfrow = c(1,1))
-barplot(marital$skutecznosc, names.arg = marital$stan)
+barplot(marital$skutecznosc, names.arg = marital$stan, 
+        ylab = "Skutecznoœæ marketingu",
+        main = "Skutecznoœæ marketingu ze wzglêdu na status matrymonialny")
 #widaæ ¿e married rzadziej decyduj¹ siê na pozyczke
 #bierzemy stan cywilny do modelu jak najbardziej
 
@@ -145,10 +151,11 @@ education <- sqldf(
 education$skutecznosc <- education$udane / education$liczba_prob
 
 par(mfrow = c(1,1))
-barplot(education$skutecznosc, names.arg = education$wyksztalcenie)
-
-#powiem szczerze tego siê nie spodziewa³em: 
+barplot(education$skutecznosc, names.arg = education$wyksztalcenie,
+        ylab = "Skutecznioœæ marketingu",
+        main = "Skutecznioœæ marketingu ze wzglêdu na wykszta³cenie")
 #ludzie z wykszta³ceniem wy¿szym czêœciej zgadzali siê na po¿yczkê
+#mo¿na siê by³o tego spodziewaæ, powinni mieæ wiêksza zdolnoœæ kredytowa
 #zmienna jaknajbardziej istotna przy tworzeniu modelu
 
 #analiza defaultu - posiadania kredytu, któego nie jest siê w stanie sp³aciæ
@@ -175,7 +182,9 @@ default <- sqldf(
 default$skutecznosc <- default$udane / default$liczba_prob
 
 par(mfrow = c(1,1))
-barplot(default$skutecznosc, names.arg = default$bankrut)
+barplot(default$skutecznosc, names.arg = default$bankrut,
+        ylab = "Skutecznoœæ marketingu",
+        main = "Skutecznoœæ marketingu ze wzglêdu na kredyt niemo¿liwy do sp³acenia")
 
 #tu te¿ jestem zdziwiony, spodziewa³bym siê, ¿e ludzie którzy ju¿
 #wczeœniej zawiedli w sp³acaniu kredytu bêd¹ siê mniej decydowaæ
@@ -228,9 +237,11 @@ balance_all$skutecznosc <- balance_all$liczba_zgod / balance_all$liczba_prob
 
 par(mfrow=c(1,2))
 barplot(balance_all$skutecznosc, names.arg = balance_all$balance,
-        xlab = "Roczny zysk w euro", ylab = "Skutecznoœæ telemarketingu")
+        xlab = "Roczny zysk w euro", ylab = "Skutecznoœæ telemarketingu",
+        main = "Skutecznoœæ marketingu ze wzglêdu na zysk roczny")
 barplot(balance_all$liczba_prob, names.arg = balance_all$balance,
-        xlab = "Roczny zysk w euro", ylab =  "Liczba prób telemarketingowych")
+        xlab = "Roczny zysk w euro", ylab =  "Liczba prób telemarketingowych",
+        main = "Rozk³ad prób marketingu ze wzglêdu na zysk roczny")
 
 #prób dla ludzi z olbrzymim zyskiem 10000+ jest malo dlatego s¹dzimy, ¿e du¿a skuecznoœæ dla nich jest przypadkowa
 #warto zwróciæ uwagê, ¿e jest du¿o prób dla ludzi z ma³¹ srat¹ lub zyskiem do 5 tysiêcy
@@ -264,7 +275,8 @@ housing <- sqldf(
 housing$skutecznosc <- housing$udane / housing$liczba_prob
 
 par(mfrow = c(1,1))
-barplot(housing$skutecznosc, names.arg = housing$hipoteka)
+barplot(housing$skutecznosc, names.arg = housing$hipoteka,
+        main = "Skutecznoœæ marketingu ze wzglêdu na posiadanie kredytu hipotecznego")
 #widaæ ¿e skutecznoœæ marketingu jest o wiele wiêksza w przypadku
 #rozmówcóW, którzy nie maj¹ na g³woie kredytu hipotecznego
 
@@ -314,7 +326,9 @@ housing_balance_yes$skutecznosc <- housing_balance_yes$`count(y)`/
                                    housing_balance_no$`count(y)`)
 par(mfrow = c(1, 1))
 barplot(housing_balance_yes$skutecznosc, 
-        names.arg = paste(housing_balance_yes$housing, housing_balance_yes$balance_level, sep="-"))
+        names.arg = paste(housing_balance_yes$housing, housing_balance_yes$balance_level, sep="-"),
+        ylab = "Skutecznoœæ marketingu",
+        main = "Skutecznoœæ marketingu ze wzglêdu posiadanie hipoteki i poziom zysku rocznego")
 
 #warto zwróciæ uwagê, ¿e chêtnymi klientami s¹ ludzie
 #o œrednim (w tym mid-low i mid-high) poziomie bilansu rocznego
@@ -346,7 +360,9 @@ loan <- sqldf(
 loan$skutecznosc <- loan$udane / loan$liczba_prob
 
 par(mfrow = c(1,1))
-barplot(loan$skutecznosc, names.arg = loan$pozyczka)
+barplot(loan$skutecznosc, names.arg = loan$pozyczka,
+        ylab = "Skutecznoœæ marketingu",
+        main = "Skutecznoœæ marketingu ze wzglêdu na posiadanie po¿yczki")
 #nie ma niespodzianki, ¿e ludzie którzy obecnie nie maj¹ ¿adnej po¿yczki, bior¹ 1. chetniej
 
 #sprawdŸmy uwzgledniaj¹c niemo¿liwoœc sp³aty (defaulty)
@@ -376,7 +392,9 @@ loan_default_yes$skutecznosc <- loan_default_yes$liczba_wynikow/
      loan_default_no$liczba_wynikow)
 par(mfrow = c(1, 1))
 barplot(housing_balance_yes$skutecznosc, 
-        names.arg = paste(housing_balance_yes$housing, housing_balance_yes$balance_level, sep="-"))
+        names.arg = paste(housing_balance_yes$housing, housing_balance_yes$balance_level, sep="-"),
+        ylab = "Skutecznoœæ marketingu",
+        main = "Skutecznoœæ marketingu ze wzglêdu na posiadanie po¿yczki lub utopionego kredytu")
  
 #niestety nie mo¿na wyci¹gn¹æ ¿adnego wniosku na temat wyró¿nienia szczególnej
 #grupy rozmówców na podstawie par loan-default
@@ -404,7 +422,9 @@ contact <- sqldf(
 contact$skutecznosc <- contact$udane / contact$liczba_prob
 
 par(mfrow=c(1,1))
-barplot(contact$skutecznosc, names.arg = contact$kontakt)
+barplot(contact$skutecznosc, names.arg = contact$kontakt,
+        ylab = "Skutecznoœæ marketingu", 
+        main = "Skutecznoœæ marketingu ze wzglêdu na formê kontaktu")
 #nie wygl¹da na to aby forma kontaktu mia³¹ wp³yw na wynik telemarketingu
 #pocz¹tkowo zmienna nie zostanie wykorzystana w modelu
 
@@ -429,8 +449,10 @@ day <- sqldf(
   on q1.dzien = q2.dzien"
 )
 day$skutecznosc <- day$udane / day$liczba_prob
-pie(day$skutecznosc, labels = day$dzien)
+pie(day$skutecznosc, labels = day$dzien,
+    main = "Skutecznoœæ marketingu w zale¿noœci od dnia miesi¹ca")
 #1, 14, 24 i 30 dzieñ miesi¹ca wyró¿niaj¹ siê wiêksz¹ skutecznoœci¹ telemarketingu
+#1 dnia by³o ma³o kontaktów, wiêc prognoza moze byæ zbyt optymistyczna
 #mo¿ê to byæ zwi¹zane z cyklem wyp³acania wynagrodzeñ
 month <- sqldf(
   "select q1.miesiac as miesiac,
@@ -452,11 +474,11 @@ month <- sqldf(
   on q1.miesiac = q2.miesiac"
 )
 month$skutecznosc <- month$udane / month$liczba_prob
-pie(month$skutecznosc, labels = day$dzien)
-#skutecznoœæ jest zdecydowanie wiêksza na prze³omie roku: listopad, grudzieñ styczeñ
-#byæ mo¿e terminy sp³at czegoœ, wydatki œwi¹teczne
+pie(month$skutecznosc, labels = month$miesiac)
+#skutecznoœæ jest zdecydowanie wiêksza w marcu, wrzeœniu, paŸdzierniku i grudniu
+#w grudniu mamy tylko 20 prób, wiêc to mo¿e byæ przyczyn¹ du¿ej skutecznoœci w tym miesi¹cy
 #du¿a skutecznoœæ w marcu: byæ mo¿e po¿yczki zwi¹zane z karnawa³em (ja bym nie bra³, ale kto wie)
-#du¿a skutecznoœæ w sierpniu: byæ mo¿e po¿yczki na wakacje, urlopy
+#du¿a skutecznoœæ we wrzeœniu i paŸdzierniku: powrót po urlopach, mo¿e kredyty studenckie
 
 day_month_yes <- sqldf(
   "select month as miesiac,
@@ -505,17 +527,42 @@ sqldf(
          and skutecznosc <= .05
    order by skutecznosc asc"
 )
+
 #istniej¹ dni które wyró¿niaj¹ siê wysok¹ skutecznoœci¹ i takie dla których skutecznoœæ jest znikoma.
+#sprawdzenie jak rozk³adaj¹ siê zawody ludzi bior¹cych po¿yczki we wrzeœniu i paŸdzierniku:
+job_month <- sqldf(
+  "select miesiac,
+          zawod,
+          count(*) as liczba_wynikow
+   from
+  (  select month as miesiac,
+            job as zawod
+     from basedata
+     where month in ('sep', 'oct')
+           and y = 'yes') t1
+   group by miesiac, zawod"
+)
+par(mfrow=c(1,2))
+pie(job_month$liczba_wynikow[job_month$miesiac == 'sep'], 
+    labels = job_month$zawod[job_month$miesiac == 'sep'],
+    main = "Rozk³ad zawodów ludzi bior¹cych po¿yczki we wrzeœniu")
+pie(job_month$liczba_wynikow[job_month$miesiac == 'oct'], 
+    labels = job_month$zawod[job_month$miesiac == 'oct'],
+    main = "Rozk³ad zawodów ludzi bior¹cych po¿yczki w paŸdzierniku")
+#okazuje siê ¿e strza³ ¿e brane s¹ po¿yczki studenckie by³ nieuzasadniony
+#wiêkszoœci¹ w obu przypadkach s¹ ludzie pracujacy przy zarz¹dzaniu
 #terminy kontaktu jak najbardziej zostan¹ wziête do modelu, najwy¿ej zostan¹ wyprune'owane
 
 #analiza wp³ywu czasu trwania ostatniej rozmowy
 par(mfrow=c(1,2))
 hist(basedata$duration[basedata$y == 'no'], xlab = "D³ugoœæ rozmowy w sekundach",
-     ylab = "Liczba odmów")
+     ylab = "Liczba odmów", main = "Rozk³ad czasu trawnia rozmowy w razue odmowy")
 hist(basedata$duration[basedata$y == 'yes'], xlab = "D³ugoœæ rozmowy w sekundach",
-     ylab = "Liczba zgód")
+     ylab = "Liczba zgód", main = "Rozk³ad czasu trwania rozmowy w razie zgody")
 
 #widaæ, ¿e w przypadku wziêcia po¿yczki rozmowy z konsultantem by³y d³u¿sze
+#ludzie nie zgadzali siê szybciej niz w 5 minut d³ugie rozmowy przynosi³y zgody
+#ludzie odmawiaj¹cy starali siê jak najszybciej odmówiæ. Rozmowy powy¿ej 20 minut nie pomog³y
 
 #analiza wp³ywu liczby kontaktów w ramach kampanii
 campaign <- sqldf(
@@ -544,9 +591,11 @@ par(mfrow=c(1,1))
 plot(campaign$liczba_kontaktow[campaign$liczba_prób >= 15], 
      campaign$skutecznosc[campaign$liczba_prób >= 15],
      xlab = "Liczba kontaktóW w ramach kampanii marketingowej",
-     ylab = "Skutecznoœæ")
+     ylab = "Skutecznoœæ",
+     main = "Skutecznoœæ marketingu w zale¿noœci od liczy kontaktów w ramach kampanii")
 
 #mo¿na dostrzec ma³¹ tendencjê, ¿e skutecznoœæ maleje wraz ze wzrostem liczby kontaktów
+#mêczenie klienta telefonami nie pomaga
 
 #analiza wp³ywu czasu pomiêdzy, który up³yn¹³ od ostatniego kontaktu z poprzedniej kampanii
 #zbijamy przerwy na przedzia³y kategoryczne w celu ³atwiejszej analizy
@@ -556,4 +605,141 @@ pdays <- sqldf(
    from basedata
    where pdays <> -1"
 )
-par(mfrow=c(1,1))
+
+par(mfrow=c(1,2))
+hist(pdays$pdays[pdays$y == 'no'], 
+     xlab = "liczba dni pomiedzy kampaniami marketingowymi",
+     ylab = "czêstoœæ odmów",
+     main = "Rozk³ad odmów w zale¿noœci od d³ugoœci przerwy miedzy kampaniami")
+hist(pdays$pdays[pdays$y == 'yes'], 
+     xlab = "liczba dni pomiedzy kampaniami marketingowymi",
+     ylab = "czêstoœæ zgód",
+     main = "Rozk³ad zgód w zale¿noœci od d³ugoœci przerwy miedzy kampaniami")
+#zdecydowana wiêkszoœæ ludzi zgadzaj¹cych siê na po¿yczkê decyduje siê w odstêpie mniejszym ni¿ 200 dni
+#zestawmy to z wynikiem poprzedniej kampanii
+poutcome <- sqldf(
+  "select poutcome,
+          pdays,
+          y
+   from basedata
+   where pdays <>-1
+         and poutcome <> 'other'"
+)
+windows()
+par(mfrow=c(2,2))
+hist(poutcome$pdays[poutcome$y == 'no' & poutcome$poutcome == 'failure'], 
+     xlab = "liczba dni pomiêdzy kampaniami marketingowymi",
+     ylab = "liczba odmów obecnie", 
+     main = "Rozk³ad odmów ze wzglêdu na czas od poprzedniej odmowy")
+hist(poutcome$pdays[poutcome$y == 'no' & poutcome$poutcome == 'success'], 
+     xlab = "liczba dni pomiêdzy kampaniami marketingowymi",
+     ylab = "liczba odmów obecnie", 
+     main = "Rozk³ad odmów ze wzglêdu na czas od poprzedniej zgody")
+hist(poutcome$pdays[poutcome$y == 'yes' & poutcome$poutcome == 'failure'], 
+     xlab = "liczba dni pomiêdzy kampaniami marketingowymi",
+     ylab = "liczba zgód obecnie", 
+     main = "Rozk³ad zgód ze wzglêdu na czas od poprzedniej odmowy")
+hist(poutcome$pdays[poutcome$y == 'yes' & poutcome$poutcome == 'success'], 
+     xlab = "liczba dni pomiêdzy kampaniami marketingowymi",
+     ylab = "liczba zgód obecnie", 
+     main = "Rozk³ad zgód ze wzglêdu na czas od poprzedniej zgody")
+#widaæ ¿e klienci s¹ doœæ statyczni: ma³o klientów zmieni³o stanowisko od czasu nastêpnej kampanii
+#zdecydowana wiêkszoœæ klientów którzy zdecydowali siê odmówiæ (przy poprzedniej zgodzie)
+#zgodzi³a siê po kontakcie po przerwie 50-200 dni lub! 300-350 dni
+#najwiêcej ludzi którzy ponownie wzieli po¿yczkê zrobi³o to po kontakcie po 50-100 dniach lub 150-200 dniach
+#nie bior¹ te¿ po¿yczek od razu, najwyraŸniej próbuj¹ siê zorientowaæ w promocjach itd
+#nak³adniaj¹c ludzi do wziêcia po¿yczki mimo uprzedniej odmowy kampanie po przerwie 400 dni nie przynios³y efektów
+#najwiêcej ludzi zmieni³o zdanie jeœli ponowny kontakt wyst¹ci³ pomiêdzy 100 a 200 dniach
+#przy tej wartoœci przerwy ogólnie najwiêcej klientów zgadza siê na po¿yczkê
+
+#spróbujmy znaleŸc do cechuje klientów którzy zmienili zdanie i zgodzili siê na po¿yczkê
+changed <- sqldf(
+  "select *
+   from basedata
+   where poutcome = 'failure'
+         and y = 'yes'
+         and pdays <> - 1"
+)
+#analizuj¹c tê tabelê rzuca siê w oczy ¿e: 
+#   prawie nikt z nich nie ma po¿yczki - loan = 'no'
+#   prawie ka¿dy kontakt by³ na komórkê 'cellular'
+#   nikt z nich nie ma utopionego kredytu
+sqldf(
+  "select education as wyksztalcenie,
+          y as wynik,
+          count(y) as liczba_wynikow
+   from changed
+   group by wyksztalcenie, y
+   order by liczba_wynikow desc"
+)
+#widzimy ¿e najczêœciej zmieniaj¹ zdanie ludzie wykszta³ceni, moze pozostali nie maj¹ zdolnoœci kredytowej
+#sprawdzmy poziom zarobków wœród tych ludzi
+changed$poziom_zysku <- sqldf(
+  "select case when balance < 0 then 'debt'
+               when balance between 0 and 500 then 'low'
+               when balance between 501 and 1000 then 'med-low'
+               when balance between 1001 and 2000 then 'med'
+               when balance between 2001 and 3000 then 'med-high'
+               when balance between 3000 and 4000 then 'high'
+               when balance > 4000 then 'huge'
+          end poziom_zysku
+   from changed"
+)$poziom_zysku
+
+sqldf(
+  "select poziom_zysku,
+          y as wynik,
+          count(y) as liczba_zgod
+   from changed
+   where education in ('secondary', 'tertiary')
+   group by poziom_zysku, y
+   order by liczba_zgod desc"
+)
+#a jednak po¿yczki bior¹ raczej ludzie którzy s¹ wykszta³ceni ale maj¹ niski poziom zysku rocznego
+sqldf(
+  "select housing as czy_hipoteka,
+          count(*) as liczba_zgod
+   from changed
+   group by czy_hipoteka
+   order by liczba_zgod desc"
+)
+#nieznaczna wiêkszoœæ z nich ma hipotekê
+#jednak nie mo¿na jednoznacznie wskazaæ dominuj¹cy w¹ski typ cz³owieka który zmieni³ zdanie i wzi¹³ po¿yczkê
+
+#sprawdzmy jeszcze wp³yw liczby kampanii 
+prev <- sqldf(
+  "select previous as liczba_kampanii,
+          count(y) as liczba_prób
+   from basedata
+   where previous > 0
+   group by liczba_kampanii
+   order by liczba_kampanii"
+)
+prev <- sqldf(
+  "select t1.*,
+          t2.liczba_udanych
+   from prev as t1
+   left join (select previous as liczba_kampanii,
+                     count(y) as liczba_udanych
+              from basedata
+              where y = 'yes'
+              group by liczba_kampanii) as t2
+        on t1.liczba_kampanii = t2.liczba_kampanii
+   order by liczba_kampanii
+")
+prev$liczba_udanych[is.na(prev$liczba_udanych) == 1] <- 0
+prev$skutecznosc <- prev$liczba_udanych/prev$liczba_prób
+par(mfrow=c(1,2))
+plot(prev$liczba_kampanii, prev$skutecznosc,
+     xlab = "Liczba poprzedzaj¹cych kampanii",
+     ylab = "Skutecznoœæ bie¿¹cej kampanii",
+     main = "Analiza wp³ywu liczby kampanii poprzedzaj¹cych")
+plot(prev$liczba_kampanii, prev$liczba_prób,
+     xlab = "Liczba poprzedzaj¹cych kampanii",
+     ylab = "Liczba bie¿¹cych prób przy x kampaniach poprzedzaj¹cych",
+     main = "Rozk³ad liczby prób o danej liczbie kampanii poprzedzaj¹cych")
+#do 6. kampanii poprzedzaj¹cych mamy du¿o próbek, wiêc analiza mo¿e byæ wiarygodna
+#mo¿na zauwa¿yæ ma³¹ tendencjê wzrostow¹ - klieci s¹ lojalni, a przynajmniej Ci od 6 kampanii
+#a im wiêcej ich przebyli tym wiêksza skutecznoœæ marketingu
+#dla wiekszych liczb kampanii mamy ma³o prób, ale raczej spodziewalibyœmy siê, ¿e nie skutkuj¹
+#i w³asnie dlatego nie by³y podejmowane
